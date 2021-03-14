d61b9fd94fe28a2e02e9338a170b84c614ba12fe
tigir-dev <aslanbm99@hotmail.com>
2021-03-14 20:20:14 +0300

    port number is set

--- src/main/java/project/App.java
+++ src/main/java/project/App.java
@@ -37,6 +37,12 @@ public class App {
         return "Hello world.";
     }
     public static void main(String[] args) {
+        Logger logger = LogManager.getLogger(App.class);
+
+        int port = Integer.parseInt(System.getenv("PORT"));
+        port(port);
+        logger.error("Current port number:" + port);
+
         port(getHerokuAssignedPort());
 
         get("/", (req, res) -> "Hello, World");
